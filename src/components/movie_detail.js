c59f5d7a4b71930c2af16d0aea6a91dcf9451fe2
Devin Hart <32463419+devin-hart@users.noreply.github.com>
2018-07-28 14:25:10 -0400

    Change MovieDetail to stateless function

--- src/components/movie_detail.js
+++ src/components/movie_detail.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class MovieDetail extends Component {
-  render() {
-    return (
-      <div>
-        { this.props.title && <h1>{this.props.title}</h1> }
-        { this.props.tagline && <h3>{this.props.tagline}</h3> }
-        { this.props.overview && <p>{this.props.overview}</p>}
-        { this.props.release_date && <p>{this.props.release_date}</p> }
-        { this.props.vote_average && <p>{this.props.vote_average}</p> }
-        { this.props.poster_path && <img src={`https://image.tmdb.org/t/p/w500/${this.props.poster_path}`} alt=""/> }
-        { this.props.error && <h2>{ this.props.error }</h2> }
+const MovieDetail = (props) => {
+  return (
+    <div>
+        { props.title && <h1>{props.title}</h1> }
+        { props.tagline && <h3>{props.tagline}</h3> }
+        { props.overview && <p>{props.overview}</p>}
+        { props.release_date && <p>{props.release_date}</p> }
+        { props.vote_average && <p>{props.vote_average}</p> }
+        { props.poster_path && <img src={`https://image.tmdb.org/t/p/w500/${props.poster_path}`} alt=""/> }
+        { props.error && <h2>{ props.error }</h2> }
       </div>
-    );
-  }
-};
+  );
+}
 
 export default MovieDetail;
