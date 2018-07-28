364f7486b5782df8412c798da2dfb73a338e2e90
Devin Hart <32463419+devin-hart@users.noreply.github.com>
2018-07-28 14:10:26 -0400

    Working poster path

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ class App extends Component {
     vote_average: undefined,
     release_date: undefined,
     poster_path: undefined,
-    genres: undefined,
     runtime: undefined,
     error: undefined
   }
@@ -23,29 +22,52 @@ class App extends Component {
     const movie = event.target.elements.movie.value;
     const apiCall = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${movie}`);
     const data = await apiCall.json();
-    console.log(data);
+    
 
-    this.setState({
-      title: data.title,
-      tagline: data.tagline,
-      overview: data.overview,
-      vote_average: data.vote_average,
-      release_date: data.release_data,
-      poster_path: data.poster_path,
-      genres: data.genres.name,
-      runtime: data.runtime,
-      
-    })
+    if (movie) {
+      console.log(data);
+      this.setState({
+        title: data.results[0].title,
+        tagline: data.results[0].tagline,
+        overview: data.results[0].overview,
+        vote_average: data.results[0].vote_average,
+        release_date: data.results[0].release_date,
+        poster_path: data.results[0].poster_path,
+        runtime: data.results[0].runtime,
+        error: ''
+      });
+    } else {
+      this.setState({
+        title: undefined,
+        tagline: undefined,
+        overview: undefined,
+        vote_average: undefined,
+        release_date: undefined,
+        poster_path: undefined,
+        runtime: undefined,
+        error: 'Movie not found.'
+    });
+  }
   }
   
   render() {
     return (
       <div className="App">
         <SearchBar getMovie={this.getMovie}/>
-        <MovieDetail />
+        <MovieDetail 
+          title={this.state.title}
+          tagline={this.state.tagline}
+          overview={this.state.overview}
+          vote_average={this.state.vote_average}
+          release_date={this.state.release_date}
+          poster_path={this.state.poster_path}
+          runtime={this.state.runtime}
+          error={this.state.error}
+          />
       </div>
     );
   }
 }
+
 
 export default App;
--- src/components/movie_detail.js
+++ src/components/movie_detail.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const MovieDetail = () => {
-  return <div>Test Movie Detail</div>
-}
+class MovieDetail extends Component {
+  render() {
+    return (
+      <div>
+        { this.props.title && <h1>{this.props.title}</h1> }
+        { this.props.tagline && <h3>{this.props.tagline}</h3> }
+        { this.props.overview && <p>{this.props.overview}</p>}
+        { this.props.release_date && <p>{this.props.release_date}</p> }
+        { this.props.vote_average && <p>{this.props.vote_average}</p> }
+        { this.props.poster_path && <img src={`https://image.tmdb.org/t/p/w500/${this.props.poster_path}`} alt=""/> }
+        { this.props.error && <h2>{ this.props.error }</h2> }
+      </div>
+    );
+  }
+};
 
 export default MovieDetail;
