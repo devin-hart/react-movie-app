6c4fb66cd8f52ce3934bca151a9a1bb913acb299
Devin Hart <32463419+devin-hart@users.noreply.github.com>
2018-07-28 17:56:48 -0400

    CSS Setup Grid

--- src/components/movie_detail.js
+++ src/components/movie_detail.js
@@ -3,10 +3,19 @@ import React from 'react';
 const MovieDetail = (props) => {
   return (
     <div>
+      <div className="col-1">
+        { props.poster_path && 
+          <img src={`https://image.tmdb.org/t/p/w500/${props.poster_path}`} 
+          alt={`${props.title} Movie Poster`}
+          className="poster" /> 
+        }
+      </div>
+
+      <div className="col-2">
         { props.title && <h1 className="movie-title">{props.title}</h1> }
         { props.tagline && <h3>{props.tagline}</h3> }
-        { props.overview && <p>{props.overview}</p>}
-        
+        { props.overview && <p className="overview">{props.overview}</p>}
+
         { props.release_date && 
           <p className="release-date">
             <strong>Release Date: </strong>{props.release_date}
@@ -19,14 +28,9 @@ const MovieDetail = (props) => {
           </p> 
         }
 
-        { props.poster_path && 
-          <img src={`https://image.tmdb.org/t/p/w500/${props.poster_path}`} 
-          alt={`${props.title} Movie Poster`}
-          className="poster" /> 
-        }
-        
         { props.error && <h2>{ props.error }</h2> }
       </div>
+    </div>
   );
 }
 
